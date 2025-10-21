432a3308deb19537a573bfaad2987213e8f842ca
Payden Hoskins <paydenhoskins@gmail.com>
2025-10-21 11:22:05 -0600

    Add project files.

---
+++ LAB9/ControlPanel.Designer.vb
@@ -0,0 +1,93 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ControlPanel
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ConnectButton = New System.Windows.Forms.Button()
+        Me.TimerButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(372, 356)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(75, 23)
+        Me.ExitButton.TabIndex = 2
+        Me.ExitButton.Text = "EXIT"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(348, 27)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 3
+        '
+        'ConnectButton
+        '
+        Me.ConnectButton.Location = New System.Drawing.Point(371, 272)
+        Me.ConnectButton.Name = "ConnectButton"
+        Me.ConnectButton.Size = New System.Drawing.Size(75, 22)
+        Me.ConnectButton.TabIndex = 4
+        Me.ConnectButton.Text = "Connect"
+        Me.ConnectButton.UseVisualStyleBackColor = True
+        '
+        'TimerButton
+        '
+        Me.TimerButton.Location = New System.Drawing.Point(375, 171)
+        Me.TimerButton.Name = "TimerButton"
+        Me.TimerButton.Size = New System.Drawing.Size(75, 23)
+        Me.TimerButton.TabIndex = 5
+        Me.TimerButton.Text = "Timer"
+        Me.TimerButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TimerButton)
+        Me.Controls.Add(Me.ConnectButton)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ConnectButton As Button
+    Friend WithEvents TimerButton As Button
+End Class
---
+++ LAB9/ControlPanel.vb
@@ -0,0 +1,81 @@
+﻿Imports System.IO.Ports
+
+Public Class ControlPanel
+    Public timerState As Integer
+    Sub Connect()
+        SerialPort1.Close()
+        SerialPort1.BaudRate = 9600
+        SerialPort1.Parity = Parity.None
+        SerialPort1.StopBits = StopBits.One
+        SerialPort1.DataBits = 8
+        Try
+            If ComboBox1.SelectedItem.ToString Is Nothing Then
+                MsgBox("Select a valid COM port", MsgBoxStyle.Critical)
+            ElseIf ComboBox1.SelectedItem.ToString <> Nothing Then
+                SerialPort1.PortName = ComboBox1.SelectedItem.ToString
+            End If
+        Catch ex As Exception
+            MsgBox("Select a valid COM port", MsgBoxStyle.Critical)
+        End Try
+
+        SerialPort1.Open()
+
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        SerialPort1.Close()
+        Me.Close()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim data(1) As Byte
+        data(0) = &H24 ' ASCII code for $
+        Select Case timerState
+            Case 1 : data(1) = &B1
+            Case 2 : data(1) = &B10
+            Case 3 : data(1) = &B11
+            Case 4 : data(1) = &B100
+            Case 5 : data(1) = &B101
+            Case 6 : data(1) = &B110
+            Case 7 : data(1) = &B111
+            Case 8 : data(1) = &B1000
+            Case 9 : data(1) = &B1001
+            Case 10 : data(1) = &B1010
+            Case 11 : data(1) = &B1011
+            Case 12 : data(1) = &B1100
+            Case 13 : data(1) = &B1101
+            Case 14 : data(1) = &B1110
+            Case 15 : data(1) = &B1111
+            Case 16 : data(1) = &B10000
+            Case 17 : data(1) = &B10001
+            Case 18 : data(1) = &B10010
+            Case 19 : data(1) = &B10011
+            Case 20 : data(1) = &B10100
+        End Select
+
+        timerState += 1
+        If timerState > 25 Then timerState = 1
+
+        SerialPort1.Write(data, 0, 2)
+    End Sub
+
+    Private Sub ConnectButton_Click(sender As Object, e As EventArgs) Handles ConnectButton.Click
+        Connect()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        For port = 0 To SerialPort.GetPortNames.Length - 1
+            ComboBox1.Items.Add(SerialPort.GetPortNames()(port))
+        Next
+    End Sub
+
+    Private Sub TimerButton_Click(sender As Object, e As EventArgs) Handles TimerButton.Click
+        If Timer1.Enabled = False Then
+            Timer1.Enabled = True
+            TimerButton.Text = "Stop Timer"
+        Else Timer1.Enabled = True
+            Timer1.Enabled = False
+            TimerButton.Text = "Start Timer"
+        End If
+    End Sub
+End Class
