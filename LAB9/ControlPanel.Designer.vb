6f4fefecda6faf510e5c559ef1601682a02fd31b
Payden Hoskins <paydenhoskins@gmail.com>
2025-10-27 10:59:42 -0600

    Final changes

--- LAB9/ControlPanel.Designer.vb
+++ LAB9/ControlPanel.Designer.vb
@@ -30,6 +30,12 @@ Partial Class ControlPanel
         Me.ConnectButton = New System.Windows.Forms.Button()
         Me.TimerButton = New System.Windows.Forms.Button()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.PositionTrackBar = New System.Windows.Forms.TrackBar()
+        Me.PositionLabel = New System.Windows.Forms.Label()
+        Me.ADCCheckBox = New System.Windows.Forms.CheckBox()
+        Me.ADCTextBox = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.PositionTrackBar, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Timer1
@@ -38,7 +44,7 @@ Partial Class ControlPanel
         '
         'ExitButton
         '
-        Me.ExitButton.Location = New System.Drawing.Point(371, 415)
+        Me.ExitButton.Location = New System.Drawing.Point(713, 415)
         Me.ExitButton.Name = "ExitButton"
         Me.ExitButton.Size = New System.Drawing.Size(75, 23)
         Me.ExitButton.TabIndex = 2
@@ -48,14 +54,14 @@ Partial Class ControlPanel
         'ComboBox1
         '
         Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Location = New System.Drawing.Point(348, 315)
+        Me.ComboBox1.Location = New System.Drawing.Point(12, 12)
         Me.ComboBox1.Name = "ComboBox1"
         Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
         Me.ComboBox1.TabIndex = 3
         '
         'ConnectButton
         '
-        Me.ConnectButton.Location = New System.Drawing.Point(261, 415)
+        Me.ConnectButton.Location = New System.Drawing.Point(12, 49)
         Me.ConnectButton.Name = "ConnectButton"
         Me.ConnectButton.Size = New System.Drawing.Size(75, 22)
         Me.ConnectButton.TabIndex = 4
@@ -64,7 +70,7 @@ Partial Class ControlPanel
         '
         'TimerButton
         '
-        Me.TimerButton.Location = New System.Drawing.Point(486, 414)
+        Me.TimerButton.Location = New System.Drawing.Point(713, 12)
         Me.TimerButton.Name = "TimerButton"
         Me.TimerButton.Size = New System.Drawing.Size(75, 23)
         Me.TimerButton.TabIndex = 5
@@ -73,18 +79,73 @@ Partial Class ControlPanel
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(609, 414)
+        Me.Button1.Location = New System.Drawing.Point(713, 49)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 6
         Me.Button1.Text = "Button1"
         Me.Button1.UseVisualStyleBackColor = True
         '
+        'PositionTrackBar
+        '
+        Me.PositionTrackBar.BackColor = System.Drawing.Color.Black
+        Me.PositionTrackBar.LargeChange = 1
+        Me.PositionTrackBar.Location = New System.Drawing.Point(12, 110)
+        Me.PositionTrackBar.Maximum = 20
+        Me.PositionTrackBar.Name = "PositionTrackBar"
+        Me.PositionTrackBar.Orientation = System.Windows.Forms.Orientation.Vertical
+        Me.PositionTrackBar.Size = New System.Drawing.Size(45, 328)
+        Me.PositionTrackBar.TabIndex = 7
+        Me.PositionTrackBar.Value = 1
+        '
+        'PositionLabel
+        '
+        Me.PositionLabel.AutoSize = True
+        Me.PositionLabel.Location = New System.Drawing.Point(12, 94)
+        Me.PositionLabel.Name = "PositionLabel"
+        Me.PositionLabel.Size = New System.Drawing.Size(75, 13)
+        Me.PositionLabel.TabIndex = 8
+        Me.PositionLabel.Text = "Servo Position"
+        '
+        'ADCCheckBox
+        '
+        Me.ADCCheckBox.AutoSize = True
+        Me.ADCCheckBox.Location = New System.Drawing.Point(63, 415)
+        Me.ADCCheckBox.Name = "ADCCheckBox"
+        Me.ADCCheckBox.Size = New System.Drawing.Size(94, 17)
+        Me.ADCCheckBox.TabIndex = 9
+        Me.ADCCheckBox.Text = "ADC SAMPLE"
+        Me.ADCCheckBox.UseVisualStyleBackColor = True
+        '
+        'ADCTextBox
+        '
+        Me.ADCTextBox.Font = New System.Drawing.Font("Consolas", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ADCTextBox.Location = New System.Drawing.Point(334, 210)
+        Me.ADCTextBox.Name = "ADCTextBox"
+        Me.ADCTextBox.ReadOnly = True
+        Me.ADCTextBox.Size = New System.Drawing.Size(100, 32)
+        Me.ADCTextBox.TabIndex = 11
+        Me.ADCTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(370, 194)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(29, 13)
+        Me.Label1.TabIndex = 12
+        Me.Label1.Text = "ADC"
+        '
         'ControlPanel
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ADCTextBox)
+        Me.Controls.Add(Me.ADCCheckBox)
+        Me.Controls.Add(Me.PositionLabel)
+        Me.Controls.Add(Me.PositionTrackBar)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.TimerButton)
         Me.Controls.Add(Me.ConnectButton)
@@ -92,7 +153,9 @@ Partial Class ControlPanel
         Me.Controls.Add(Me.ExitButton)
         Me.Name = "ControlPanel"
         Me.Text = "Form1"
+        CType(Me.PositionTrackBar, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
@@ -103,4 +166,9 @@ Partial Class ControlPanel
     Friend WithEvents ConnectButton As Button
     Friend WithEvents TimerButton As Button
     Friend WithEvents Button1 As Button
+    Friend WithEvents PositionTrackBar As TrackBar
+    Friend WithEvents PositionLabel As Label
+    Friend WithEvents ADCCheckBox As CheckBox
+    Friend WithEvents ADCTextBox As TextBox
+    Friend WithEvents Label1 As Label
 End Class
--- LAB9/ControlPanel.vb
+++ LAB9/ControlPanel.vb
@@ -1,7 +1,12 @@
-﻿Imports System.IO.Ports
+﻿Option Explicit On
+Option Strict On
+
+Imports System.IO.Ports
 
 Public Class ControlPanel
+    Public waitTimer As Integer = 0
     Public timerState As Integer
+    Public data(1) As Byte
     Sub Connect()
         SerialPort1.Close()
         SerialPort1.BaudRate = 9600
@@ -34,24 +39,24 @@ Public Class ControlPanel
         Select Case timerState
             Case 1 : data(0) = &H55
             Case 2 : data(0) = &HAA
-                '    Case 3 : data(1) = &B11
-                '    Case 4 : data(1) = &B100
-                '    Case 5 : data(1) = &B101
-                '    Case 6 : data(1) = &B110
-                '    Case 7 : data(1) = &B111
-                '    Case 8 : data(1) = &B1000
-                '    Case 9 : data(1) = &B1001
-                '    Case 10 : data(1) = &B1010
-                '    Case 11 : data(1) = &B1011
-                '    Case 12 : data(1) = &B1100
-                '    Case 13 : data(1) = &B1101
-                '    Case 14 : data(1) = &B1110
-                '    Case 15 : data(1) = &B1111
-                '    Case 16 : data(1) = &B10000
-                '    Case 17 : data(1) = &B10001
-                '    Case 18 : data(1) = &B10010
-                '    Case 19 : data(1) = &B10011
-                '    Case 20 : data(1) = &B10100
+            Case 3 : data(1) = &B11
+            Case 4 : data(1) = &B100
+            Case 5 : data(1) = &B101
+            Case 6 : data(1) = &B110
+            Case 7 : data(1) = &B111
+            Case 8 : data(1) = &B1000
+            Case 9 : data(1) = &B1001
+            Case 10 : data(1) = &B1010
+            Case 11 : data(1) = &B1011
+            Case 12 : data(1) = &B1100
+            Case 13 : data(1) = &B1101
+            Case 14 : data(1) = &B1110
+            Case 15 : data(1) = &B1111
+            Case 16 : data(1) = &B10000
+            Case 17 : data(1) = &B10001
+            Case 18 : data(1) = &B10010
+            Case 19 : data(1) = &B10011
+            Case 20 : data(1) = &B10100
         End Select
 
         timerState += 1
@@ -85,10 +90,12 @@ Public Class ControlPanel
         Connect()
     End Sub
 
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Private Sub ControlPanel_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         For port = 0 To SerialPort.GetPortNames.Length - 1
             ComboBox1.Items.Add(SerialPort.GetPortNames()(port))
         Next
+        ComboBox1.SelectedIndex = 0
+        Connect()
     End Sub
 
     Private Sub TimerButton_Click(sender As Object, e As EventArgs) Handles TimerButton.Click
@@ -102,29 +109,105 @@ Public Class ControlPanel
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim data(1) As Byte
-        'data(0) = &HAA ' ASCII code for $
-        'data(1) = &H55
-        Select Case timerState
-            Case 0 : timerState = 1
-                data(0) = &H24
-            Case 1 : data(0) = &H24
-            Case 2 : data(0) = &H24
+        SendServoData()
+    End Sub
+
+    Sub ADCDATA()
+        Dim wait%
+        Dim aWhile1$ = "0"
+        Dim aWhile$ = "0"
+        Dim Value(1) As Integer
+        If ADCCheckBox.Checked = True Then
+            data(0) = &H21
+            If SerialPort1.IsOpen Then
+                SerialPort1.Write(data, 0, 1)
+
+                Do Until aWhile IsNot "0" Or aWhile1 IsNot "0"
+                    Dim ADC(SerialPort1.BytesToRead) As Byte
+                    Try
+                        SerialPort1.Read(ADC, 0, SerialPort1.BytesToRead)
+                        aWhile = CStr(ADC(1))
+                        aWhile1 = CStr(ADC(0))
+                        Value(0) = ADC(0)
+                        Value(1) = ADC(1)
+                    Catch ex As Exception
+
+                    End Try
+                    wait += 1
+                    If wait = 1000 Then
+                        ADCTextBox.Text = CStr($"0,0")
+                        Exit Do
+                    End If
+                Loop
+                Try
+                    ADCTextBox.Text = CStr($"{(0 + 1 * (Value(0) * 256) + Value(1)) / 2}")
+                Catch ex As Exception
+                    MsgBox("Bad Data.")
+                End Try
+
+            End If
+        End If
+
+    End Sub
+
+    Sub SendServoData()
+        'Connect()
+        data(0) = &H24
+        If SerialPort1.IsOpen Then
+            SerialPort1.Write(data, 0, 1)
+
+            'Read and wait for proper response befor moving on
+            Dim something As String = ""
+            Do Until something = "36"
+                Dim TX(SerialPort1.BytesToRead) As Byte
+                SerialPort1.Read(TX, 0, SerialPort1.BytesToRead) 'read bytes from
+                something = CStr($"{TX(0)}")
+                waitTimer += 1
+                'give it enough time to recive data
+                If waitTimer = 800 Then
+                    waitTimer = 0
+                    Exit Do
+                End If
+            Loop
+            waitTimer = 0
+            If something = "36" Then
+                Console.WriteLine("hooray!")
+                Servo_Position()
+                SerialPort1.Write(data, 0, 1)
+            End If
+        End If
+
+    End Sub
+
+
+    Sub Servo_Position()
+        Select Case PositionTrackBar.Value
+            Case 0 : data(0) = &B0
+            Case 1 : data(0) = &B1
+            Case 2 : data(0) = &B10
+            Case 3 : data(0) = &B11
+            Case 4 : data(0) = &B100
+            Case 5 : data(0) = &B101
+            Case 6 : data(0) = &B110
+            Case 7 : data(0) = &B111
+            Case 8 : data(0) = &B1000
+            Case 9 : data(0) = &B1001
+            Case 10 : data(0) = &B1010
+            Case 11 : data(0) = &B1011
+            Case 12 : data(0) = &B1100
+            Case 13 : data(0) = &B1101
+            Case 14 : data(0) = &B1110
+            Case 15 : data(0) = &B1111
+            Case 16 : data(0) = &B10000
+            Case 17 : data(0) = &B10001
+            Case 18 : data(0) = &B10010
+            Case 19 : data(0) = &B10011
+            Case 20 : data(0) = &B10100
         End Select
+    End Sub
 
-        timerState += 1
-        If timerState > 2 Then timerState = 1
-
-        SerialPort1.Write(data, 0, 1)
-
-        'Read and wait for proper response befor moving on
-        Dim something As String = ""
-        Do Until something = "36"
-            Dim TX(SerialPort1.BytesToRead) As Byte
-            SerialPort1.Read(TX, 0, SerialPort1.BytesToRead) 'read bytes from
-            something = CStr($"{TX(0)}")
-        Loop
-        Console.WriteLine("hooray!")
-        Connect()
+    Private Sub PositionTrackBar_ValueChanged(sender As Object, e As EventArgs) Handles PositionTrackBar.ValueChanged
+        SendServoData()
+        ADCDATA()
     End Sub
 End Class
